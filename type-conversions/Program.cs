4fc3c1e302660c2518dcbc98846945fe0306e750
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-08-03 19:06:52 +0300

    Implement implicit and explicit conversions; sample with other operator overloading

--- type-conversions/Program.cs
+++ type-conversions/Program.cs
@@ -6,7 +6,15 @@ namespace type_conversions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point(100, 200);
+            string coords = p1;
+            Console.WriteLine(coords);
+
+            Point p2 = (Point)coords;
+            Console.WriteLine($"Point: ({p2.X}, {p2.Y})");
+
+            Point p3 = p1 + p2;
+            Console.WriteLine($"Point sum: ({p3.X}, {p3.Y})");
         }
     }
 }
